679cbd96bc67bcb5dc6b67f4f1ea1bfa31eafe58
moonyu37 <yuzu.tink02@gmail.com>
2023-05-14 21:15:35 +0900

    画像のみでもアイテム投稿可能となるバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  validates :item_name, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
